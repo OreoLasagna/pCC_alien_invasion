8fcad4fdf0042d3ce6ca531f3a43e588774c135d
git <bpdaut@outlook.com>
2025-03-29 10:31:53 -0500

    Added a comment for versioning

--- settings.py
+++ settings.py
@@ -7,3 +7,5 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+#Adding an extra comment here for the sake of versioning

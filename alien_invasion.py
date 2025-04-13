97c3baab5ca257c7b60899e1fb3d5b400ca5b669
git <bpdaut@outlook.com>
2025-04-13 10:25:52 -0500

    Refactored the update bullet method, split it out, and messed with bullet settings some.

--- alien_invasion.py
+++ alien_invasion.py
@@ -108,6 +108,23 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
             #print(len(self.bullets))
 
+        self._check_bullet_alien_collisions()
+
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions"""
+         #Check for any bullets that have hit aliens
+        #If so, get rid of the bullet and the alien
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True
+        )
+
+        if not self.aliens:
+            #Destroy existing bullets and create new fleet
+            self.bullets.empty()
+            self._create_fleet()
+
+
     def _create_fleet(self):
         """Create the fleet of aliens"""
         #Create an alien and keep adding aliens until there's no room left

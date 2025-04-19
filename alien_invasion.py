080a09398f341a5457fc01bd619ce0b542a9c491
git <bpdaut@outlook.com>
2025-04-19 15:55:49 -0500

    Building out high score functionality and making the scores rounded numbers. Points from shooting down aliens increments each time the screen refreshes

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame # type: ignore #Can just ignore the squiggles this works
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -28,8 +29,10 @@ class AlienInvasion:
 
         pygame.display.set_caption("Alien Invasion")
 
-        #Create an instance to store game statistics
+        #Create an instance to store game statistics,
+        #and create a scoreboard
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -91,6 +94,7 @@ class AlienInvasion:
     def _start_game(self):
         #Reset the game statistics.
             self.stats.reset_stats()
+            self.sb.prep_score()
             self.game_active = True
 
             #Get rid of any remaining bullets and aliens
@@ -166,6 +170,11 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True
         )
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
 
         if not self.aliens:
             #Destroy existing bullets and create new fleet
@@ -268,6 +277,9 @@ class AlienInvasion:
             bullet.draw_bullet()
         
         self.aliens.draw(self.screen)
+
+        #Draw the score information
+        self.sb.show_score()
 
         self.ship.blitme()
 
